8670fb40e3c9141f0050c5dfd4fedb9b4cb3d698
Pavel <19paul91@mail.ru>
2016-06-05 13:33:43 +0300

    add files torch app

---
+++ Torch/Torch/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Torch
+//
+//  Created by Павел Анплеенко on 04/06/16.
+//  Copyright © 2016 Pavel Anpleenko. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+	var images = ["torchon.jpg", "torchoff.jpg"]
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	@IBAction func torchButton(sender: AnyObject) {
+			let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+				
+			if device.hasTorch{
+				do{
+					try device.lockForConfiguration()
+					device.torchMode = device.torchMode == AVCaptureTorchMode.On ? .Off : .On
+					device.unlockForConfiguration()
+				} catch{
+					print(error)
+				}
+		}
+
+	}
+}
+
